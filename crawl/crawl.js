38eecda1bb1f1ebf7f85673d6f00bf5a3e98d5e1
Anders Sjöberg <shoobah@gmail.com>
2015-06-09 00:08:45 +0200

    Modified the crawler a bit

--- crawl/crawl.js
+++ crawl/crawl.js
@@ -1,19 +1,48 @@
 import Crawler from 'js-crawler'
 
+let myUrl = process.argv[2];
+let depth = 2;
+let myPages = [];
+let otherPages = [];
+let n = 0;
+
 let crawler = new Crawler().configure({
     ignoreRelative: false,
-    depth: 2
+    depth: depth
 })
 
+if (typeof String.prototype.startsWith !== 'function') {
+    String.prototype.startsWith = function(str) {
+        return this.slice(0, str.length) == str;
+    }
+}
+
+function progress(text) {
+    process.stdout.write(text + '\r');
+}
+
 crawler.crawl({
-    url: process.argv[2],
+    url: myUrl,
     success: function(page) {
-        console.log(page.url);
+        debugger;
+        if (page.url.startsWith(myUrl)) {
+            console.log(page.url);
+            myPages.push(page);
+            n += 1;
+        } else {
+            otherPages.push(page);
+        }
     },
-    failure: function(page) {
-        console.warn(page.status);
+    failed: function(page) {
+        otherPages.push(page)
     },
     finished: function(crawledUrls) {
         console.log(`Krälade igenom ${crawledUrls.length} sidor`);
+        console.log(`Sparade ${myPages.length} sidor`);
+        console.log('-------------------------------------------------')
+        for (var i = otherPages.length - 1; i >= 0; i--) {
+            console.log(otherPages[i].url);
+        };
+        console.log('-------------------------------------------------')
     }
 })
